390ae415b988a2d72d5d80237d963d37dee5ede4
twugo <hm17031@student.miyazaki-u.ac.jp>
2021-10-08 00:22:41 +0900

    Update: コードを整理、調整

--- Assets/MentosCola/Hand/HandMover.cs
+++ Assets/MentosCola/Hand/HandMover.cs
@@ -8,7 +8,7 @@ namespace MentosCola {
         Action[] MoveList;
 
         // 今回の動き、ランダムで設定
-        int thisTimeMove;
+        int thisTimeMove = 0;
 
         [SerializeField] Vector3 startPosition = new Vector3(-10, 5, 0);
 
@@ -21,40 +21,15 @@ namespace MentosCola {
         // Start is called before the first frame update
         void Awake() {
             MoveList = new Action[]{
-            // UniformLinearMove,
-            SimpleVibrationMove,
-            WaveMove,
-            CircularMove
-        };
-            Reset();
+                WaveMove,
+                SimpleVibrationMove,
+                CircularMove
+            };
         }
 
         // Update is called once per frame
         void FixedUpdate() {
             MoveList[thisTimeMove]();
-        }
-
-        // /// <summary>
-        // /// 等速直線運動、反復あり
-        // /// </summary>
-        // void UniformLinearMove(){
-        //     float x = this.transform.position.x;
-        //     float speed = 0.03f;
-        //     if(x < 5){
-        //         transform.Translate(speed, 0, 0, Space.World);
-        //     }else {
-        //         transform.Translate(-speed, 0, 0, Space.World);
-        //         Debug.Log(-speed);
-        //     }
-        // }
-
-        /// <summary>
-        /// 単振動
-        /// </summary>
-        void SimpleVibrationMove() {
-            float time = Time.time;
-            float amplitude = 8f;
-            transform.position = new Vector3(amplitude * Mathf.Sin(Time.time), startPosition.y, 0);
         }
 
         /// <summary>
@@ -68,12 +43,21 @@ namespace MentosCola {
         }
 
         /// <summary>
+        /// 単振動
+        /// </summary>
+        void SimpleVibrationMove() {
+            float time = Time.time / 2;
+            float amplitude = 8f;
+            transform.position = new Vector3(amplitude * Mathf.Sin(time), startPosition.y, 0);
+        }
+
+        /// <summary>
         /// 円運動
         /// </summary>
         void CircularMove() {
             float time = Time.time;
-            float radius_x = 5;
-            float radius_y = 2;
+            float radius_x = 8;
+            float radius_y = 4;
             transform.position = new Vector3(radius_x * Mathf.Cos(time), radius_y * Mathf.Sin(time) + radius_y + startPosition.y, 0);
         }
     }
