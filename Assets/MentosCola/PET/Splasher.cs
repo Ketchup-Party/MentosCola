2707d19b312eaa90f33b6abdcf814a00e20a75a5
twugo <hm17031@student.miyazaki-u.ac.jp>
2021-10-10 15:29:55 +0900

    Update: パーティクルを結果に応じて色々変わるようにした

--- Assets/MentosCola/PET/Splasher.cs
+++ Assets/MentosCola/PET/Splasher.cs
@@ -13,12 +13,34 @@ namespace MentosCola {
 
         [SerializeField] GameOnePlayLoopManager gameOnePlayLoopManager = default;
         [SerializeField] ParticleSystem splashParticle = default;
+        /// <summary>
+        /// パーティクルを吹き出す。
+        /// </summary>
         void Splash() {
             OneTrialResult result = gameOnePlayLoopManager.ChangeToSplash();
 
+            // パーティクルの初速
+            float startSpeed = result.GetDistanceWhenRelease() * 10;
+
+            // パーティクルの色
+            float hue = (Mathf.Max(0, ((float)result.GetElapsedTimeMilliseconds() - 1000)) / 100) % 1.0f;
+            Color startColorMin = Color.HSVToRGB(hue, 1, 1);
+            Color startColorMax = Color.HSVToRGB((hue + 0.4f) % 1.0f, 1, 1);
+
+            // パーティクルの数
+            float rateOverTime = result.GetFirstSpeed() * 100;
+
+            // 初速の設定
             var main = splashParticle.main;
-            int startSpeed = result.GetScore() / 300;
             main.startSpeed = new ParticleSystem.MinMaxCurve(startSpeed);
+
+            // 色の設定
+            main.startColor = new ParticleSystem.MinMaxGradient(startColorMin, startColorMax);
+
+            // 数の設定
+            var emission = splashParticle.emission;
+            float rateOverTimeMinCorrectionValue = 0.75f;
+            emission.rateOverTime = new ParticleSystem.MinMaxCurve(rateOverTimeMinCorrectionValue * rateOverTime, rateOverTime);
 
             splashParticle.Play();
         }
